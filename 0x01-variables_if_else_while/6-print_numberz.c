49f6105c04fb20ff013bec7827e9e7908db0025b
Saviour-Dagadu <savicane@gmail.com>
2023-06-18 21:15:16 -0700

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  *main - Prints numbers from 0 to 9
  *
  *Return: Always 0 (Success);
 */
+
 int main(void)
 {
 	int i;
-	for (i = 48; i < 58; i++)
+	for (i = '0'; i < '9'; i++)
 	{
 		putchar(i);
 	}
