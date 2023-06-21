5f072199ccb8ff2aa741f032b3a522228d2606ab
Saviour-Dagadu <savicane@gmail.com>
2023-06-20 21:07:35 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Retru: void
+*/
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = '0'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
