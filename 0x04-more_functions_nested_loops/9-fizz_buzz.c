3eb99ad214aa24a461042529ad7d42ec032043b7
Saviour-Dagadu <savicane@gmail.com>
2023-06-22 23:59:39 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,15 +9,14 @@
  * and FizzBuzz for multiples of both
  *
  * Return: always 0
-
-*/
+ */
 
 int main(void)
 {
 	int i;
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
@@ -33,7 +32,7 @@ int main(void)
 		{
 			printf("%i", i);
 		}
-		if (i < 100)
+		if (i != 100)
 		{
 			printf(" ");
 		}
