9c557001011ddd6edfad0ef43ebdc12dbe8736bd
Saviour-Dagadu <savicane@gmail.com>
2023-06-26 05:18:33 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int: Takes in two variables integers and swap them
- * @author: Savicode
+ * swap_int: takes in two variables integers and swap them
  * @a: swap and stores address of b
  * @b: swaps and stores address of a
  * Return: 0
@@ -11,6 +10,7 @@
 void swap_int(int *a, int *b)
 {
 	int change;
+
 	change = *b;
 	*b = *a;
 	*a = change;
