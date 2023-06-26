273000af493074a4102d3e9a3b6cd3d85ae2e7e8
Saviour-Dagadu <savicane@gmail.com>
2023-06-26 04:55:32 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int: Takes in two variables integers and swap them
+ * @author: Savicode
+ * @a: swap and stores address of b
+ * @b: swaps and stores address of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int change;
+	change = *b;
+	*b = *a;
+	*a = change;
+}
