80d58734cdc7c0ef2a8fd5f59bdee6012b589e54
Saviour-Dagadu <savicane@gmail.com>
2023-08-11 12:58:35 -0700

    changes

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,11 +6,12 @@
  *main - Prints the lowercase alphabet in reverse.
  *followed by a new line
  *Return: Always 0 (Success);
-*/
+ */
 
 int main(void)
 {
 	char ch;
+
 	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
