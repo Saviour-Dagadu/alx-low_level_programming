d2906a112b18928bb7615b072361ca6560d8a5d1
Saviour-Dagadu <savicane@gmail.com>
2023-08-12 00:10:58 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,10 +4,11 @@
  * main - prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i, j;
-	
+
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
